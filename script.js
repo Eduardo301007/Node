b3386079592b71cc3b94e09d6d8985808281ea44
Eduardo301007 <edu.ferreira3010@gmail.com>
2026-04-11 12:54:09 -0300

    Criação do usuário

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const dbUser = process.env.DB_USER
 const dbPass = process.env.DB_PASS
 //Pegando o banco de dados
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.aze2pmq.mongodb.net/?appName=Cluster0`)
-.then(()=>{
+.then(()=>{ //mesma coisa que o try except só que para código ASSÍNCRONO
 app.listen(3000, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
@@ -41,26 +41,48 @@ app.listen(3000, () => {
 
 //Criando rostas(ending points)
 app.get("/", (req, res) =>{
-    res.json({users});
+    res.json({nome, email, senha, confirmsenha});
 })
 
-app.post("/login", (req, res) =>{
+app.post("/login", async (req, res) =>{
     const {nome, email, senha, confirmsenha} = req.body
 
     //Validação dos dados
     if(!nome || !email || !senha){
         return res.status(422).json({mgs: "Coloque os dados obrigatórios"}) 
     }
-    else if(senha !== confirmsenha){
+    if(senha !== confirmsenha){
         return res.status(422).json({mgs: "As senhas não são iguais"}) 
     }
-    else{
-        return res.status(422).json({mgs: "Tudo certin"}) 
-    }
+    
 
     //Checar se o usuário existe
-    const checkuser = User.findOne({email: email})//Semelhante à uma WHERE
-    
+    const checkuser = await User.findOne({email: email})//Semelhante à uma WHERE. Procura na lista de usuários se tem um email correspondente ao email antes colocado
+
+    if (checkuser){
+        return res.status(422).json({msg: 'Por favor, utilize outro email'}) //Se já existir um usuário com este email quando for criar um usuário, vai retornar esta mensagem
+    }
+
+    //Criando uma senha com digitos a mais na senha que o usuário digitou (nesse caso 12 dígitos aleatórios a mais)
+    const salt = await bcrypt.genSalt(12)
+    const senhahash = await bcrypt.hash(senha, salt)
+
+    //Criar usuário
+    const user = new User({
+        nome,
+        email, 
+        senha: senhahash,
+    })
+
+    try{
+        await user.save()//Vai salvar o usuário no banco de dados
+        res.status(201).json({msg:'Usuário salvo com sucesso'})
+    }
+    catch(er){
+        res.status(500).json({msg: 'Erro no servidor'})
+    }
+
+     
     
 })
 
